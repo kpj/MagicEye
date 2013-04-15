737b413e8967b931a1f8b7c7d83e720fcf91db23
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-04-15 16:15:01 +0200

    updated commit.sh

--- commit.sh
+++ commit.sh
@@ -1,10 +1,17 @@
 msg="$1"
 
+# make sure we are on master branch
+git checkout master
+
+# add latest changes
 git add .
 git commit -am"$msg"
 
+# also apply them in gh-pages
 git checkout gh-pages
 git merge master
+git push
 
+# and finally in master
 git checkout master
 git push
